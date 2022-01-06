64104e4196d5cad9350d24c290eb95c8d117829b
Wanting Wang <wanting.wang@columbia.edu>
2022-01-05 23:02:13 -0500

    Update check_and_deploy_kubectl_new.sh

--- scripts/check_and_deploy_kubectl_new.sh
+++ scripts/check_and_deploy_kubectl_new.sh
@@ -69,6 +69,8 @@ spec:
       dnsPolicy: ClusterFirst
       securityContext: {}
       schedulerName: default-scheduler
+  imagePullSecrets:
+    - name: all-icr-io
   strategy:
     type: RollingUpdate
     rollingUpdate:
